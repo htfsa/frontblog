6e73be1d348dc3b2bd16fea0099c4676db8994c8
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-11-24 12:03:24 -0300

    Material Ui

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import { Title } from './components/Title/Title';
 import { Text } from './components/Text';
+import Home from './components/paginas/home/Home';
 
 function App() {
   const nome = 'Herbert';
   return (
-    <>
-      <Title nome={nome} />
-      <Text />
-    </>
+   <Home />
   );
 }
 
