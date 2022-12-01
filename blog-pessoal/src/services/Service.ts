5094d22e78d57ddea21e229cef80ab043bb500d0
Herbert Ferreira da Silva azevedo <112709078+kakarotofla@users.noreply.github.com>
2022-12-01 15:54:08 -0300

    introduction of login and registration

--- blog-pessoal/src/services/Service.ts
+++ blog-pessoal/src/services/Service.ts
@@ -1,10 +1,14 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const api = axios.create({
-    baseURL:'https://bloggeneration.herokuapp.com'
+    baseURL: 'https://bloggeneration.herokuapp.com'
 })
-export const login = async(url, dados, setDado) =>{
+    export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data)
+    }
 
-    const resposta = await api.post(url, dados)
-    setDado(resposta.data)
-}
+    export const login = async(url: any,dados: any,setDado: any) => { 
+        const resposta = await api.post(url,dados)
+        setDado(resposta.data.token)
+    }
